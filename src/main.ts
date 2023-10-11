fcf89f1da6a34f3555a477abb86013389f62d7db
AdongoJr2 <dev.mosesadongo@gmail.com>
2023-10-11 14:38:17 +0300

    Configure database
    - Used PostgreSQL + TypeORM

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // setting global path prefix
+  app.setGlobalPrefix('api/v1');
+
+  // enable cors
+  app.enableCors();
+
+  await app.listen(PORT, () => {
+    console.log(`Server Listening on PORT: ${PORT}`);
+  });
 }
 bootstrap();
